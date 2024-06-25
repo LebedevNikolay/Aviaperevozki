4de901d8e2c4e8de35eacd47c63a81f9c5eabdca
nikolay <nikolay-kmc@mail.ru>
2024-06-25 19:46:15 +0300

    main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 11404; //стоимость билета
+        int bonus = 20; //сумма одной бонусной мили
+
+        int miles;  //Переменная мили.
+        if (ticketPrice < 0) {
+            System.out.printf("Стоимость билета не может быть отрицательной"); //проверка отрицательной стоимости билета
+        } else if (ticketPrice > 0) {
+            miles = ticketPrice / bonus;//расчет количества миль, за покупку билета.
+            System.out.printf("Кол-во миль за покупку билета: " + miles);// Вывод результата.
+
+        }
+    }
+}
